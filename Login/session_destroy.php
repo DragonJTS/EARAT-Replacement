4ef611be1fafb5acd0c5317170ce6091efb1ec57
Jake <dragonjts@gmail.com>
2013-02-18 17:17:16 -0500

    Fixed comment

--- Login/session_destroy.php
+++ Login/session_destroy.php
@@ -1,10 +1,10 @@
 <?php
 /*
-* Jake Marshall
-* Feb 18, 2013
-* This destorys a users session, and sends them to the login page
-* Used for logging out.
-*/
+ * Jake Marshall
+ * Feb 18, 2013
+ * This destroys a user's session, and sends them to the login page
+ * Used for logging out.
+ */
 session_start(); //creates session
 session_destroy(); //destroys session. Yes, it must be created first
 header("location:login.php"); //redirects to login
